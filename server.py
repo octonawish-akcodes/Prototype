bf5aa9bd7633d79bfa68812bfe222a293871e137
Abhishek Kumar <abhishek22512@gmail.com>
2023-06-05 23:02:46 +0530

    fix

--- server.py
+++ server.py
@@ -7,9 +7,6 @@ serverPort = 8080
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/plain")
-        self.end_headers()
         
         encoded_url = self.path[1:]  # Remove the leading '/' from the encoded URL
         
@@ -17,10 +14,15 @@ class MyServer(BaseHTTPRequestHandler):
         
         path = cache.fetch_file(url)
         if path:
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
             with open(path, "br") as file:
                 self.wfile.write(file.read())
         else:
-            self.wfile.write(b"")  # Empty response if URL is not in cache
+            self.send_response(302)
+            self.send_header("Location", url)
+            self.end_headers()
 
 if __name__ == "__main__":
     webServer = HTTPServer((hostName, serverPort), MyServer)
